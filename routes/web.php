d99f8b6d1f0cfadd6ff7cef5d8f0ea87afea7070
Davide <davide.roberti89@gmail.com>
2020-05-25 16:00:02 +0200

    aggiunto scaffolding per auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'postController@index')->name('posts.index');
 
 Route::resource('posts', 'postController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
